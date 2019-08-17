26aff342ff7ec349deaa1c838d247283585b8a21
Matteo Bartalini <MatteoBartalini1@gmail.com>
2019-08-17 13:00:06 -0400

    Insecticide for dark mode + find bug

--- Textor/Controller/DocumentViewController.swift
+++ Textor/Controller/DocumentViewController.swift
@@ -479,7 +479,7 @@ class DocumentViewController: UIViewController {
 		textStorage.removeAttribute(NSAttributedString.Key.foregroundColor, range: range)
 		//if statement is needed here so that cursor doesn't move for non-coding mode
 		if syntaxLanguage != nil { //only needs to be done if highlightr is on
-			textView.text = textView.text //while stupid, this is the only way for us to manually call the highlightr
+			textStorage.language = textStorage.language //while stupid, this is the best way for us to manually call the highlightr (most reliable)
 		} else if UserDefaultsController.shared.isDarkMode { //if no syntax highlighting add some color back
 			textStorage.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.white, range: range)
 		} else { //isLightMode
